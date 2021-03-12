6054b015e7a34a63f580fc7258a9f43c80ff8ad8
J. Daian d'Esposito <daian.desposito@mercadolibre.com>
2021-03-12 17:57:54 -0300

    MOD ResponseDto & CharacterDto

--- SpringBoot/starwars/src/main/java/com/bootcamp/starwars/controller/StarWarsController.java
+++ SpringBoot/starwars/src/main/java/com/bootcamp/starwars/controller/StarWarsController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -17,7 +16,8 @@ class StarWarsRestController
     private ICharacterFinderService finderService;
 
     @GetMapping(path = "/{name}")
-    public List<CharacterDTO> findCharacter(@PathVariable String name){
+    public List<CharacterDTO> findCharacter(@PathVariable String name)
+    {
         return finderService.findName(name);
     }
 }
