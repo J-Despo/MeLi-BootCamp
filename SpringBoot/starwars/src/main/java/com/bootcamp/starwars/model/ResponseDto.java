ab7ecc9d5ee4ad47db723c115c1eb89aa02ac7a5
J. Daian d'Esposito <daian.desposito@mercadolibre.com>
2021-03-15 09:55:07 -0300

    Prue

--- SpringBoot/starwars/src/main/java/com/bootcamp/starwars/model/ResponseDto.java
+++ SpringBoot/starwars/src/main/java/com/bootcamp/starwars/model/ResponseDto.java
@@ -1,16 +1,12 @@
 package com.bootcamp.starwars.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter @Setter
 public class ResponseDto
 {
     List<CharacterDTO> nameList;
-
-    public List<CharacterDTO> getNameList() {
-        return nameList;
-    }
-
-    public void setNameList(List<CharacterDTO> nameList) {
-        this.nameList = nameList;
-    }
 }
